181453305994379051273d5d280f86bb2a25e6b1
yangyixuan.112358 <yangyixuan.112358@bytedance.com>
2023-06-03 21:57:01 +0800

    Mac程序框架

---
+++ SimplePlayer/SimplePlayer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SimplePlayer
+//
+//  Created by YangYixuan on 2023/6/3.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ SimplePlayer/SimplePlayer/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SimplePlayer
+//
+//  Created by YangYixuan on 2023/6/3.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
