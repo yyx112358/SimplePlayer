b7558a1a86b48d69fceeada99776f6162c7852da
yangyixuan.112358 <yangyixuan.112358@bytedance.com>
2023-07-09 16:03:41 +0800

    GLRendererPreview支持旋转、翻转、填充模式

--- SimplePlayer/SimplePlayer/Preview/Renderer/GLRendererBase.hpp
+++ SimplePlayer/SimplePlayer/Preview/Renderer/GLRendererBase.hpp
@@ -211,49 +211,139 @@ protected:
     std::array<GLfloat, 4> _clearColor;
 };
 
-class GLRendererPreview : public GLRendererBase {
+class GLRendererPreview : public GLRendererBase
+{
 public:
-    GLRendererPreview(std::shared_ptr<GLContext> context):GLRendererBase(context) {
-        
-    }
+    /// 填充模式
+    enum class EFillMode
+    {
+        Fit,        // 【默认】适应，缩放至刚好不超出屏幕，可能有空隙。
+        Stretch,    // 拉伸，填满屏幕。可能改变长宽比。
+        Fill,       // 填充，缩放至刚好填满屏幕。不改变长宽比。
+        Origin,     // 保持原始尺寸，不缩放。
+        Free,       // 自由，由_transform变换矩阵决定
+    };
+    /// 旋转，逆时针为正方向
+    enum class ERotation
+    {
+        Rotation0,
+        Rotation90,
+        Rotation180,
+        Rotation270,
+    };
+
+public:
+    GLRendererPreview(std::shared_ptr<GLContext> context):GLRendererBase(context) {}
     virtual ~GLRendererPreview() {}
     
-    virtual bool UpdatePreviewSize(int width, int height) {
-        _width = width;
-        _height = height;
-        return true;
+    void UpdatePreviewSize(int width, int height)
+    {
+        _previewWidth = width;
+        _previewHeight = height;
+    }
+    
+    void UpdatePreviewFillMode(EFillMode fillmode)
+    {
+        _fillmode = fillmode;
+    }
+    
+    void UpdatePreviewRotation(ERotation rotation)
+    {
+        _rotation = rotation;
+    }
+    
+    void UpdatePreviewFlip(bool flipX, bool flipY)
+    {
+        _flipX = flipX;
+        _flipY = flipY;
     }
     
 protected:
-    bool _InternalUpdate() override {
+    virtual void _UpdateTransform()
+    {
+        if (_textures.size() == 0 || _fillmode == EFillMode::Free)
+            return;
+        
+        GLfloat texWidth = _textures[0]->width(), texHeight = _textures[0]->height();
+        if (_rotation == ERotation::Rotation90 || _rotation == ERotation::Rotation270)
+            std::swap(texWidth, texHeight);
+        GLfloat scaleX = 1, scaleY = 1;
+        switch(_fillmode) {
+            case EFillMode::Fit:
+                if (GLfloat scale = _previewWidth / texWidth; texHeight * scale <= _previewHeight) {
+                    scaleX = scale;
+                    scaleY = scale;
+                } else {
+                    scaleX = _previewHeight / texHeight;
+                    scaleY = _previewHeight / texHeight;
+                }
+                break;
+                
+            case EFillMode::Stretch:
+                scaleX = _previewWidth / texWidth;
+                scaleY = _previewHeight / texHeight;
+                break;
+                
+            case EFillMode::Fill:
+                if (GLfloat scale = _previewWidth / texWidth; texHeight * scale >= _previewHeight) {
+                    scaleX = scale;
+                    scaleY = scale;
+                } else {
+                    scaleX = _previewHeight / texHeight;
+                    scaleY = _previewHeight / texHeight;
+                }
+                break;
+                
+            case EFillMode::Origin:
+                scaleX = 1;
+                scaleY = 1;
+                break;
+                
+            case EFillMode::Free:
+                return;
+        }
+        if (_flipX)
+            scaleX *= -1;
+        if (_flipY)
+            scaleY *= -1;
+        
+        _transform = glm::identity<glm::mat4>();
+        _transform = glm::scale(_transform, glm::vec3(scaleX, scaleY, 1.0f));   // 缩放到preview区域内
+        _transform = glm::scale(_transform, glm::vec3(texWidth / _previewWidth, texHeight / _previewHeight, 1.0f)); // 归一化
+        _transform = glm::rotate(_transform, glm::pi<GLfloat>() / 2 * (int)_rotation, glm::vec3(0.0f, 0.0f, 1.0f)); // 旋转
+    }
+    
+    bool _InternalUpdate() override
+    {
         if (_needUpdate == false)
             return true;
         
         UpdateShader({R"(
-#version 330 core
-layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec2 aTexCoords;
+        #version 330 core
+        layout (location = 0) in vec2 aPos;
+        layout (location = 1) in vec2 aTexCoords;
 
-out vec2 TexCoords;
+        uniform mat4 transform;
+        out vec2 TexCoords;
 
-void main()
-{
-    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
-    TexCoords = aTexCoords;
-}
-)"}, {R"(
-#version 330 core
-out vec4 FragColor;
+        void main()
+        {
+            gl_Position = transform * vec4(aPos.x, aPos.y, 0.0, 1.0);
+            TexCoords = aTexCoords;
+        }
+        )"}, {R"(
+        #version 330 core
+        out vec4 FragColor;
 
-in vec2 TexCoords;
+        in vec2 TexCoords;
 
-uniform sampler2D screenTexture;
+        uniform sampler2D screenTexture;
 
-void main()
-{
-    FragColor = texture(screenTexture, TexCoords);
-}
-)"});
+        void main()
+        {
+            FragColor = texture(screenTexture, TexCoords);
+        }
+        )"});
         _program->Activate();
         _vertexArray.Activate();
         
@@ -261,17 +351,25 @@ void main()
         return true;
     }
     
-    bool _InternalRender() override {
+    bool _InternalRender() override
+    {
         glBindFramebuffer(GL_FRAMEBUFFER, 0); // 绑定到屏幕
-        glViewport(0, 0, _width, _height);
+        glViewport(0, 0, _previewWidth, _previewHeight);
         glClearColor(1, 1, 1, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        assert(_textures.size() > 0);
+        if (_textures.size() == 0)
+            return false;
         
         _program->Activate();
         _vertexArray.Activate();
         glActiveTexture(GL_TEXTURE0 + 0); // 激活纹理单元1
         glBindTexture(GL_TEXTURE_2D, *_textures[0]->id()); // 绑定纹理。根据上下文，这个纹理绑定到了纹理单元1
         _program->UpdateUniform("screenTexture", 0); // 更新纹理uniform
+        
+        _UpdateTransform();
+        _program->UpdateUniform("transform", _transform); // 更新转换矩阵
         _program->FlushUniform();
         
         _vertexArray.Render();
@@ -283,7 +381,10 @@ void main()
     }
     
 protected:
-    int _width = 0, _height = 0;
+    int _previewWidth = 0, _previewHeight = 0;
+    EFillMode _fillmode = EFillMode::Fit;
+    ERotation _rotation = ERotation::Rotation0;
+    bool _flipX = false, _flipY = false;
 };
 
 class GLRendererCharPainting : public GLRendererBase {
