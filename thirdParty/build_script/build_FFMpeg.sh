3657799640ff095957bf382004593cd414a8f6c3
yangyixuan.112358 <yangyixuan.112358@bytedance.com>
2023-07-11 17:06:53 +0800

    添加FFMpeg编译脚本

---
+++ thirdParty/build_script/build_FFMpeg.sh
@@ -0,0 +1,26 @@
+echo "Build FFMpeg"
+
+SOURCE_DIR="../FFMpeg"
+BUILD_DIR="FFMpeg"
+
+cd ../build
+rm -rf $BUILD_DIR
+
+# 编译FFMpeg
+cd $SOURCE_DIR
+# ./configure --enable-debug --enable-ffplay --enable-nonfree --enable-gpl
+# make -j10
+
+# 复制header和lib
+libnames=("libavcodec" "libavdevice" "libavfilter" "libavformat" "libavutil" "libpostproc" "libswresample" "libswscale")
+
+cd ../build
+for libname in "${libnames[@]}"
+do
+    echo $libname
+    mkdir -p $BUILD_DIR/include/$libname
+    cp -r $SOURCE_DIR/$libname/*.h $BUILD_DIR/include/$libname/
+    
+    mkdir -p $BUILD_DIR/lib
+    cp $SOURCE_DIR/$libname/*.a $BUILD_DIR/lib/
+done
